077ba99d2cccd85a93d6a0ba7a705f734ca1901e
Ismail Marmoush <marmoushismail@gmail.com>
2023-08-02 23:23:46 +0200

    clean up

--- testsuite/src/main/java/io/memoria/reactive/testsuite/TestsuiteDefaults.java
+++ testsuite/src/main/java/io/memoria/reactive/testsuite/TestsuiteDefaults.java
@@ -2,7 +2,6 @@ package io.memoria.reactive.testsuite;
 
 import io.memoria.atom.core.text.SerializableTransformer;
 import io.memoria.atom.core.text.TextTransformer;
-import io.memoria.reactive.core.msg.stream.Msg;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
@@ -21,10 +20,6 @@ public class TestsuiteDefaults {
 
   public static String topicName(Class<?> tClass) {
     return "topic%d_%s".formatted(System.currentTimeMillis(), tClass.getSimpleName());
-  }
-
-  public static Msg createEsMsg(int i) {
-    return new Msg(String.valueOf(i), "hello" + i);
   }
 
   public static void printRates(String methodName, long now) {
