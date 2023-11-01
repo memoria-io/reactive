41632148e964d73b8ba5b5cbd7ea21ce5a736802
Ismail Marmoush <marmoushismail@gmail.com>
2023-11-01 22:53:38 +0100

    A Quick Commit

--- testsuite/src/main/java/io/memoria/reactive/testsuite/Main.java
+++ testsuite/src/main/java/io/memoria/reactive/testsuite/Main.java
@@ -1,7 +1,13 @@
 package io.memoria.reactive.testsuite;
 
+import io.memoria.reactive.eventsourcing.pipeline.PartitionPipeline;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+  private static final Logger log = LoggerFactory.getLogger(PartitionPipeline.class.getName());
+
   public static void main(String[] args) {
-    System.out.println("hello world");
+    log.info("Silence is golden");
   }
 }
