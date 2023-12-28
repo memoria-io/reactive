d3002f8c4d6a03789aa3c3636401cd23637db7da
Ismail Marmoush <marmoushismail@gmail.com>
2023-12-28 14:43:01 +0100

    refactoring test and clean up

--- testsuite/src/test/java/io/memoria/reactive/testsuite/Data.java
+++ testsuite/src/test/java/io/memoria/reactive/testsuite/Data.java
@@ -22,10 +22,6 @@ import java.util.function.Supplier;
 
 public class Data {
   // Data
-  public static final String alice = "alice";
-  public static StateId aliceId = StateId.of(alice);
-  public static final String bob = "bob";
-  public static StateId bobId = StateId.of(bob);
   public final Supplier<Id> idSupplier;
   public final Supplier<Long> timeSupplier;
   public final AccountSaga saga;
