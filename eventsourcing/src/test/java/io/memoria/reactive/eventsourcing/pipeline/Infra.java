7cb2e634e9f4cc10fa3c3759fe083dabbbb224b9
Ismail Marmoush <marmoushismail@gmail.com>
2024-01-13 21:02:31 +0100

    start pipeline testing

---
+++ eventsourcing/src/test/java/io/memoria/reactive/eventsourcing/pipeline/Infra.java
@@ -0,0 +1,29 @@
+package io.memoria.reactive.eventsourcing.pipeline;
+
+import io.memoria.atom.core.text.SerializableTransformer;
+import io.memoria.atom.eventsourcing.Domain;
+import io.memoria.reactive.eventsourcing.stream.MsgStream;
+import io.vavr.Tuple;
+import io.vavr.Tuple2;
+import io.vavr.collection.List;
+
+public class Infra {
+  private final SerializableTransformer transformer;
+  public final MsgStream inMemoryStream;
+
+  public Infra() {
+    this.transformer = new SerializableTransformer();
+    // streams
+    this.inMemoryStream = MsgStream.inMemory();
+  }
+
+  public List<Tuple2<CommandRoute, EventRoute>> getRoutes(int x) {
+    var cTopic = "commands" + System.currentTimeMillis();
+    var eTopic = "events" + System.currentTimeMillis();
+    return List.range(0, x).map(i -> Tuple.of(new CommandRoute(cTopic, i, x), new EventRoute(eTopic, i, x)));
+  }
+
+  public PartitionPipeline inMemoryPipeline(Domain domain, CommandRoute commandRoute, EventRoute eventRoute) {
+    return new PartitionPipeline(domain, commandRoute, eventRoute, inMemoryStream, transformer);
+  }
+}
--- eventsourcing/src/test/java/io/memoria/reactive/eventsourcing/pipeline/PartitionPipelineTest.java
+++ eventsourcing/src/test/java/io/memoria/reactive/eventsourcing/pipeline/PartitionPipelineTest.java
@@ -1,12 +1,26 @@
 package io.memoria.reactive.eventsourcing.pipeline;
 
+import io.memoria.atom.eventsourcing.StateId;
+import io.memoria.atom.testsuite.eventsourcing.command.AccountCommand;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 class PartitionPipelineTest {
+  private final Infra infra = new Infra();
+  private final Data data = Data.ofSerial();
+  private final CommandRoute commandRoute = new CommandRoute("commands");
+  private final EventRoute eventRoute = new EventRoute("events");
+  private static final int NUM_OF_ACCOUNTS = 10;
+  private static final int EXPECTED_EVENTS_COUNT = (NUM_OF_ACCOUNTS / 2) * 5;
 
   @Test
   void happyPath() {
-
+    var pipeline = infra.inMemoryPipeline(data.domain(), commandRoute, eventRoute);
+    simpleDebitProcess(NUM_OF_ACCOUNTS).concatMap(pipeline::publishCommand).subscribe();
+    StepVerifier.create(pipeline.handle().take(EXPECTED_EVENTS_COUNT))
+                .expectNextCount(EXPECTED_EVENTS_COUNT)
+                .verifyComplete();
   }
 
   @Test
@@ -32,5 +46,14 @@ class PartitionPipelineTest {
   @Test
   void invalidSequence() {
 
+  }
+
+  public Flux<AccountCommand> simpleDebitProcess(int accounts) {
+    var debitedIds = data.createIds(0, accounts / 2).map(StateId::of);
+    var creditedIds = data.createIds(accounts / 2, accounts).map(StateId::of);
+    var createDebitedAcc = data.createAccountCmd(debitedIds, 500);
+    var createCreditedAcc = data.createAccountCmd(creditedIds, 500);
+    var debitTheAccounts = data.debitCmd(debitedIds.zipWith(creditedIds), 300);
+    return createDebitedAcc.concatWith(createCreditedAcc).concatWith(debitTheAccounts);
   }
 }
