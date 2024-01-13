39ae5a15ca612926340a7f5b84090adb8a31770c
Ismail Marmoush <marmoushismail@gmail.com>
2024-01-13 16:51:12 +0100

    A Quick Commit

---
+++ eventsourcing/src/test/java/io/memoria/reactive/eventsourcing/stream/PartitionPipelineTest.java
@@ -0,0 +1,36 @@
+package io.memoria.reactive.eventsourcing.stream;
+
+import org.junit.jupiter.api.Test;
+
+class PartitionPipelineTest {
+
+  @Test
+  void happyPath() {
+
+  }
+
+  @Test
+  void duplicateEvent() {
+
+  }
+
+  @Test
+  void duplicateCommand() {
+
+  }
+
+  @Test
+  void duplicateSagaCommand() {
+
+  }
+
+  @Test
+  void wrongState() {
+
+  }
+
+  @Test
+  void invalidSequence(){
+    
+  }
+}
