7ef1d69bdd0e17d2846ee8273c8933d37c54496f
Ismail Marmoush <marmoushismail@gmail.com>
2024-01-22 21:15:23 +0100

    A Quick Commit

--- eventsourcing/src/test/java/io/memoria/reactive/eventsourcing/pipeline/PartitionPipelineTest.java
+++ eventsourcing/src/test/java/io/memoria/reactive/eventsourcing/pipeline/PartitionPipelineTest.java
@@ -2,7 +2,9 @@ package io.memoria.reactive.eventsourcing.pipeline;
 
 import io.memoria.atom.eventsourcing.StateId;
 import io.memoria.atom.testsuite.eventsourcing.command.AccountCommand;
+import io.vavr.collection.List;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
@@ -10,12 +12,14 @@ import reactor.test.StepVerifier;
 import java.time.Duration;
 
 class PartitionPipelineTest {
-  private final Infra infra = new Infra();
-  private final Data data = Data.ofSerial();
-
   private static final int NUM_OF_ACCOUNTS = 10;
   private static final int EXPECTED_EVENTS_COUNT = (NUM_OF_ACCOUNTS / 2) * 5;
   private static final Duration timeout = Duration.ofMillis(100);
+
+  private final Infra infra = new Infra();
+  private final Data data = Data.ofSerial();
+  private final Flux<StateId> debitedIds = data.createIds(0, NUM_OF_ACCOUNTS / 2).map(StateId::of);
+  private final Flux<StateId> creditedIds = data.createIds(NUM_OF_ACCOUNTS / 2, NUM_OF_ACCOUNTS / 2).map(StateId::of);
 
   @Test
   void happyPath() {
@@ -23,7 +27,7 @@ class PartitionPipelineTest {
     var pipeline = createSimplePipeline();
 
     // When
-    simpleDebitProcess(NUM_OF_ACCOUNTS).concatMap(pipeline::publishCommand).subscribe();
+    createAccounts().concatWith(debitAccounts()).concatMap(pipeline::publishCommand).subscribe();
 
     // Then
     StepVerifier.create(pipeline.handle())
@@ -33,21 +37,34 @@ class PartitionPipelineTest {
   }
 
   @Test
-  void shouldRedirectCommand() {
+  @DisplayName("Initialize should pick up last state")
+  void initialize() {
     // Given
-    var commandRoute = new CommandRoute("commands", 0, 2);
-    var eventRoute = new EventRoute("events");
-    var pipeline = infra.inMemoryPipeline(data.domain(), commandRoute, eventRoute);
-    // When
-    var cmd = data.createAccountCmd(StateId.of(0), 300);
-    var isInPartition = cmd.meta().isInPartition(commandRoute.partition(), commandRoute.totalPartitions());
+    var pipeline = createSimplePipeline();
+
+    // When creating accounts
+    createAccounts().concatMap(pipeline::publishCommand).subscribe();
+
     // Then
-    Assertions.assertThat(isInPartition).isFalse();
-    StepVerifier.create(pipeline.redirectIfNeeded(cmd)).expectComplete().verify();
+    StepVerifier.create(pipeline.handle())
+                .expectNextCount(NUM_OF_ACCOUNTS)
+                .expectTimeout(Duration.ofMillis(100))
+                .verify();
+
+    // When old pipeline died
+    pipeline = createSimplePipeline();
+    debitAccounts().concatMap(pipeline::publishCommand).subscribe();
+
+    // Then new pipeline should pick up last state
+    StepVerifier.create(pipeline.handle())
+                .expectNextCount(EXPECTED_EVENTS_COUNT)
+                .expectTimeout(Duration.ofMillis(100))
+                .verify();
   }
 
   @Test
-  void shouldNotRedirectCommand() {
+  @DisplayName("Should handle commands with assigned partition")
+  void shouldNotRedirect() {
     // Given
     var commandRoute = new CommandRoute("commands", 0, 2);
     var eventRoute = new EventRoute("events");
@@ -58,6 +75,21 @@ class PartitionPipelineTest {
     // Then
     Assertions.assertThat(isInPartition).isTrue();
     StepVerifier.create(pipeline.redirectIfNeeded(cmd)).expectNext(cmd).verifyComplete();
+  }
+
+  @Test
+  @DisplayName("Should redirect commands with unassigned partition ")
+  void shouldRedirect() {
+    // Given
+    var commandRoute = new CommandRoute("commands", 0, 2);
+    var eventRoute = new EventRoute("events");
+    var pipeline = infra.inMemoryPipeline(data.domain(), commandRoute, eventRoute);
+    // When
+    var cmd = data.createAccountCmd(StateId.of(0), 300);
+    var isInPartition = cmd.meta().isInPartition(commandRoute.partition(), commandRoute.totalPartitions());
+    // Then
+    Assertions.assertThat(isInPartition).isFalse();
+    StepVerifier.create(pipeline.redirectIfNeeded(cmd)).expectComplete().verify();
   }
 
   @Test
@@ -74,7 +106,7 @@ class PartitionPipelineTest {
   void atLeastOnceEvents() {
     // Given
     var pipeline = createSimplePipeline();
-    simpleDebitProcess(NUM_OF_ACCOUNTS).concatMap(pipeline::publishCommand).subscribe();
+    createAccounts().concatWith(debitAccounts()).concatMap(pipeline::publishCommand).subscribe();
     StepVerifier.create(pipeline.handle()).expectNextCount(EXPECTED_EVENTS_COUNT).expectTimeout(timeout).verify();
 
     // When
@@ -97,13 +129,33 @@ class PartitionPipelineTest {
   }
 
   @Test
-  void duplicateCommand() {
+  @DisplayName("Command should be dropped when it's already handled")
+  void isHandledCommand() {
+    // Given
+    var pipeline = createSimplePipeline();
 
+    // When
+    var createAccounts = createAccounts().toStream().toList();
+    Flux.fromIterable(createAccounts).concatMap(pipeline::publishCommand).subscribe();
+    StepVerifier.create(pipeline.handle()).expectNextCount(NUM_OF_ACCOUNTS).expectTimeout(timeout).verify();
+
+    // Then
+    Assertions.assertThat(pipeline.isHandledCommand(createAccounts.getFirst())).isTrue();
   }
 
   @Test
   void duplicateSagaCommand() {
+    // Given
+    var pipeline = createSimplePipeline();
 
+    // When
+    createAccounts().concatWith(debitAccounts()).concatMap(pipeline::publishCommand).subscribe();
+    StepVerifier.create(pipeline.handle()).expectNextCount(NUM_OF_ACCOUNTS).expectTimeout(timeout).verify();
+    var firstCommand = createAccounts().blockFirst();
+
+    // Then
+    assert firstCommand != null;
+    Assertions.assertThat(pipeline.isHandledSagaCommand(firstCommand)).isTrue();
   }
 
   @Test
@@ -120,12 +172,13 @@ class PartitionPipelineTest {
     return infra.inMemoryPipeline(data.domain(), new CommandRoute("commands"), new EventRoute("events"));
   }
 
-  private Flux<AccountCommand> simpleDebitProcess(int accounts) {
-    var debitedIds = data.createIds(0, accounts / 2).map(StateId::of);
-    var creditedIds = data.createIds(accounts / 2, accounts / 2).map(StateId::of);
+  private Flux<AccountCommand> createAccounts() {
     var createDebitedAcc = data.createAccountCmd(debitedIds, 500);
     var createCreditedAcc = data.createAccountCmd(creditedIds, 500);
-    var debitTheAccounts = data.debitCmd(debitedIds.zipWith(creditedIds), 300);
-    return createDebitedAcc.concatWith(createCreditedAcc).concatWith(debitTheAccounts);
+    return createDebitedAcc.concatWith(createCreditedAcc);
+  }
+
+  private Flux<AccountCommand> debitAccounts() {
+    return data.debitCmd(debitedIds.zipWith(creditedIds), 300);
   }
 }
